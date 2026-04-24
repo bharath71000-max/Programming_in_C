217091d7effb48ab7862286e018e1388963c3c47
bharath71000-max <bharath71000@gmail.com>
2026-04-24 22:00:32 +0530

    Create sum-of-array_elements.c

---
+++ sum-of-array_elements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, sum = 0;
+
+    printf("Enter 5 numbers:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];  // adding elements
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
