134c1e849917441b915c113be90ec8460ad1f2d4
bharath71000-max <bharath71000@gmail.com>
2026-05-19 23:52:43 +0530

    Create prime_no.c

---
+++ prime_no.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // Prime numbers are greater than 1
+    if (n <= 1) {
+        isPrime = 0;
+    } else {
+        // Check divisibility from 2 to n/2
+        for (i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a Prime Number\n", n);
+    else
+        printf("%d is Not a Prime Number\n", n);
+
+    return 0;
+}
